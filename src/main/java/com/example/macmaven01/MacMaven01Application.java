50b8768ed86ac25b2007cdb3becc0601fc64f8a3
ashimagarg67 <ashima.garg67@gmail.com>
2024-04-04 12:50:27 +0530

    Add files via upload

---
+++ src/main/java/com/example/macmaven01/MacMaven01Application.java
@@ -0,0 +1,13 @@
+package com.example.macmaven01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacMaven01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MacMaven01Application.class, args);
+    }
+
+}
